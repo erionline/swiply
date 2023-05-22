2eb9e0809df7accfa9f3898ca136b5328defa01e
Eray C <hi@eri.gg>
2023-05-23 00:34:05 +0200

    ref: Move user related structure to entities

---
+++ src/utils/entities/user.entity.ts
@@ -0,0 +1,35 @@
+import { atom } from 'jotai'
+
+export const userAtom = atom<UserProfile>({
+    id: "",
+    name: "",
+    picture: "",
+    bio: "",
+    email: "",
+    password: "",
+    posts: [],
+})
+
+export interface UserProfile {
+    id: string;
+    name: string;
+    picture: string;
+    bio: string;
+    email: string;
+    password?: string;
+    posts: UserPost[];
+}
+
+export interface UserPost {
+    authorId: string;
+    title: string;
+    content: string;
+    date: Date;
+    likes: number;
+    comments: UserComment[];
+}
+
+export interface UserComment {
+    content: string;
+    date: Date;
+}
